a7de415b852c1899cc856ff2ebbc71f850730ae0
kostasleo <kon.leontaridis@gmail.com>
2020-02-10 11:41:43 +0200

    main changed

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+// Branch kougias
+
 #include "Player.hpp"
 
 #include <iostream>
@@ -6,4 +8,5 @@ using namespace std;
 
 int main() {
     cout << "\n\nInitializing Game...\n\n" << endl;
+    cout << "This is kougias branch" << endl;
 }
