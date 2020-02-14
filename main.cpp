809a3de53646747bdb3e0693913732c736cfe417
AlexKougianos <48020560+AlexKougianos@users.noreply.github.com>
2020-02-14 16:17:14 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,5 +8,5 @@ using namespace std;
 
 int main() {
     cout << "\n\nInitializing Game...\n\n" << endl;
-    cout << "This is master branch" << endl;
+    cout << "This is Kougias branch" << endl;
 }
